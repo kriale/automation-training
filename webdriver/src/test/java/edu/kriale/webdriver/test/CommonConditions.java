4f42e6410c736ff8382c6192bf802ad821623964
Alexey Krytsky <alexeykrytsky@gmail.com>
2019-11-20 10:10:10 +0300

    framework: add CommonConditions class

---
+++ webdriver/src/test/java/edu/kriale/webdriver/test/CommonConditions.java
@@ -0,0 +1,27 @@
+package edu.kriale.webdriver.test;
+
+import edu.kriale.webdriver.driver.DriverSingleton;
+import edu.kriale.webdriver.util.TestListener;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+
+@Listeners({TestListener.class})
+public class CommonConditions {
+
+    protected WebDriver driver;
+
+
+    @BeforeMethod()
+    public void setUp()
+    {
+        driver = DriverSingleton.getDriver();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopBrowser()
+    {
+        DriverSingleton.closeDriver();
+    }
+}
