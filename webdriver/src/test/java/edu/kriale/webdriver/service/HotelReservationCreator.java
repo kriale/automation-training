3b7c92528bf0f817b4458a1eadae84ca688fb91a
Alexey Krytsky <alexeykrytsky@gmail.com>
2019-11-30 22:54:31 +0300

    framework: fix finals names in the HomeReservationCreator class

--- webdriver/src/test/java/edu/kriale/webdriver/service/HotelReservationCreator.java
+++ webdriver/src/test/java/edu/kriale/webdriver/service/HotelReservationCreator.java
@@ -7,13 +7,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class HotelReservationCreator {
-    private static final String TEST_DATE_CASE3_GOING_TO = "test-data.case3.going-to";
-    private static final String TEST_DATE_CASE3_CHECK_IN_PLUS_DAYS = "test-data.case3.check-in.plus-days";
-    private static final String TEST_DATE_CASE3_CHECK_OUT_PLUS_DAYS = "test-data.case3.check-out.plus-days";
+    private static final String TEST_DATA_CASE3_GOING_TO = "test-data.case3.going-to";
+    private static final String TEST_DATA_CASE3_CHECK_IN_PLUS_DAYS = "test-data.case3.check-in.plus-days";
+    private static final String TEST_DATA_CASE3_CHECK_OUT_PLUS_DAYS = "test-data.case3.check-out.plus-days";
 
-    private static final String TEST_DATE_CASE4_GOING_TO = "test-data.case4.going-to";
-    private static final String TEST_DATE_CASE4_CHECK_IN_PLUS_DAYS = "test-data.case4.check-in.plus-days";
-    private static final String TEST_DATE_CASE4_CHECK_OUT_PLUS_DAYS = "test-data.case4.check-out.plus-days";
+    private static final String TEST_DATA_CASE4_GOING_TO = "test-data.case4.going-to";
+    private static final String TEST_DATA_CASE4_CHECK_IN_PLUS_DAYS = "test-data.case4.check-in.plus-days";
+    private static final String TEST_DATA_CASE4_CHECK_OUT_PLUS_DAYS = "test-data.case4.check-out.plus-days";
 
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
@@ -21,19 +21,19 @@ public class HotelReservationCreator {
     public static HotelReservation withLessThanOneDayStayingFromProperty() {
         LocalDate nowDate = LocalDate.now();
         return (new HotelReservation())
-                .setGoingTo(TestDataReader.getTestData(TEST_DATE_CASE3_GOING_TO))
+                .setGoingTo(TestDataReader.getTestData(TEST_DATA_CASE3_GOING_TO))
                 .setReservationDates(new ReservationDates(
-                        getDateStringWithPlusDays(nowDate, TEST_DATE_CASE3_CHECK_IN_PLUS_DAYS),
-                        getDateStringWithPlusDays(nowDate, TEST_DATE_CASE3_CHECK_OUT_PLUS_DAYS)));
+                        getDateStringWithPlusDays(nowDate, TEST_DATA_CASE3_CHECK_IN_PLUS_DAYS),
+                        getDateStringWithPlusDays(nowDate, TEST_DATA_CASE3_CHECK_OUT_PLUS_DAYS)));
     }
 
     public static HotelReservation withSimpleCredentialsFromProperty(){
         LocalDate nowDate = LocalDate.now();
         return (new HotelReservation())
-                .setGoingTo(TestDataReader.getTestData(TEST_DATE_CASE4_GOING_TO))
+                .setGoingTo(TestDataReader.getTestData(TEST_DATA_CASE4_GOING_TO))
                 .setReservationDates(new ReservationDates(
-                        getDateStringWithPlusDays(nowDate, TEST_DATE_CASE4_CHECK_IN_PLUS_DAYS),
-                        getDateStringWithPlusDays(nowDate, TEST_DATE_CASE4_CHECK_OUT_PLUS_DAYS)))
+                        getDateStringWithPlusDays(nowDate, TEST_DATA_CASE4_CHECK_IN_PLUS_DAYS),
+                        getDateStringWithPlusDays(nowDate, TEST_DATA_CASE4_CHECK_OUT_PLUS_DAYS)))
                 .setHotelReservationRooms(HotelReservationRoomCreator.getOneRoomWithWithoutChildren());
     }
 
