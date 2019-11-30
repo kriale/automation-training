2be3ec3217c9377904deed75741f3ff2c9876d45
Alexey Krytsky <alexeykrytsky@gmail.com>
2019-11-30 22:54:53 +0300

    framework: migrate tests to testng

--- webdriver/src/test/java/edu/kriale/webdriver/test/HotelSearchParamsPageTests.java
+++ webdriver/src/test/java/edu/kriale/webdriver/test/HotelSearchParamsPageTests.java
@@ -2,8 +2,9 @@ package edu.kriale.webdriver.test;
 
 import edu.kriale.webdriver.service.HotelReservationCreator;
 import edu.kriale.webdriver.service.TestDataReader;
-import org.junit.jupiter.api.*;
 import edu.kriale.webdriver.pageobject.HotelsSearchParamsPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -12,13 +13,12 @@ public class HotelSearchParamsPageTests extends CommonConditions {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private static final String PROPERTY_NAME_EXPECTED_CHECK_OUT = "test-data.case3.check-out.plus-days.expected";
 
-    @Test
-    @DisplayName("test-case-3: Search hotel for less than one day staying")
+    @Test(testName = "test-case-3: Search hotel for less than one day staying")
     public void searchHotelsForLessThanOneDayStaying() {
         HotelsSearchParamsPage page = new HotelsSearchParamsPage(driver);
         page.openPage()
                 .fillFromParams(HotelReservationCreator.withLessThanOneDayStayingFromProperty());
-        Assertions.assertEquals(LocalDate.now()
+        Assert.assertEquals(LocalDate.now()
                 .plusDays(Integer.parseInt(TestDataReader.getTestData(PROPERTY_NAME_EXPECTED_CHECK_OUT)))
                 .format(FORMATTER), page.getCheckOutFieldText());
     }
--- webdriver/src/test/java/edu/kriale/webdriver/test/HotelSearchResultPageTests.java
+++ webdriver/src/test/java/edu/kriale/webdriver/test/HotelSearchResultPageTests.java
@@ -4,9 +4,8 @@ import edu.kriale.webdriver.pageobject.HotelsSearchParamsPage;
 import edu.kriale.webdriver.pageobject.HotelsSearchResultsPage;
 import edu.kriale.webdriver.service.HotelReservationCreator;
 import edu.kriale.webdriver.service.TestDataReader;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class HotelSearchResultPageTests extends CommonConditions {
     private static final String EXPECTED_EQUAL_DEPARTURE_AND_ARRIVAL_ALERT_MESSAGE =
@@ -14,17 +13,15 @@ public class HotelSearchResultPageTests extends CommonConditions {
                     "Please adjust your search or continue booking your hotel separately.";
     private static final String PROPERTY_NAME_FLYING_FROM = "test-data.case4.flying-from";
 
-    @Test
-    @DisplayName("test-case-4: Search hotel with flight from A to A")
+    @Test(testName = "test-case-4: Search hotel with flight from A to A")
     public void searchHotelsWithFlightFromAToA() {
         HotelsSearchParamsPage page = new HotelsSearchParamsPage(driver);
         HotelsSearchResultsPage resultsPage = page.openPage()
                 .fillFromParams(HotelReservationCreator.withSimpleCredentialsFromProperty())
                 .enableAddFlightCheckbox()
-                .fillFlyingFromField(TestDataReader.getTestData(TestDataReader
-                        .getTestData(PROPERTY_NAME_FLYING_FROM)))
+                .fillFlyingFromField(TestDataReader.getTestData(PROPERTY_NAME_FLYING_FROM))
                 .search();
-        Assertions.assertEquals(EXPECTED_EQUAL_DEPARTURE_AND_ARRIVAL_ALERT_MESSAGE,
+        Assert.assertEquals(EXPECTED_EQUAL_DEPARTURE_AND_ARRIVAL_ALERT_MESSAGE,
                 resultsPage.getAlertMessage());
     }
 }
