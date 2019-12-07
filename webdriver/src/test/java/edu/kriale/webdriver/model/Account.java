403993bd58b710628700c3a29403e1f60a26b131
Alexey Krytsky <alexeykrytsky@gmail.com>
2019-12-07 19:01:47 +0300

    framework: add Account model

---
+++ webdriver/src/test/java/edu/kriale/webdriver/model/Account.java
@@ -0,0 +1,57 @@
+package edu.kriale.webdriver.model;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class Account {
+    private String email;
+    private String password;
+
+
+    public Account(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Account() {
+    }
+
+
+    public Optional<String> getEmail() {
+        return Optional.ofNullable(email);
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Optional<String> getPassword() {
+        return Optional.ofNullable(password);
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(email, account.email) &&
+                Objects.equals(password, account.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
