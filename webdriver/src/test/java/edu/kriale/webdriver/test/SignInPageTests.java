1f815ae9a445693d2df7e75369dd0a5f49d53b34
Alexey Krytsky <alexeykrytsky@gmail.com>
2019-12-07 18:46:02 +0300

    framework2: create SignInPageTests class

---
+++ webdriver/src/test/java/edu/kriale/webdriver/test/SignInPageTests.java
@@ -0,0 +1,8 @@
+package edu.kriale.webdriver.test;
+
+import edu.kriale.webdriver.pageobject.HomePage;
+import org.testng.annotations.Test;
+
+public class SignInPageTests extends CommonConditions {
+
+}
