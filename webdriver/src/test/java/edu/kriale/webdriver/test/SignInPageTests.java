4ebfdc3d1b44c13713555b886121d6f0647fc9ee
Alexey Krytsky <alexeykrytsky@gmail.com>
2019-12-07 19:31:19 +0300

    framework2: add 'user sign in' test in SignInPageTests class

--- webdriver/src/test/java/edu/kriale/webdriver/test/SignInPageTests.java
+++ webdriver/src/test/java/edu/kriale/webdriver/test/SignInPageTests.java
@@ -1,8 +1,15 @@
 package edu.kriale.webdriver.test;
 
 import edu.kriale.webdriver.pageobject.HomePage;
+import edu.kriale.webdriver.service.AccountCreator;
 import org.testng.annotations.Test;
 
 public class SignInPageTests extends CommonConditions {
-
+    @Test(testName = "test-case-1: User sing in")
+    public void signUpTest() {
+        HomePage page = (new HomePage())
+                .openPage()
+                .openSignInPage()
+                .signIn(AccountCreator.withCredentialsFromProperty());
+    }
 }
