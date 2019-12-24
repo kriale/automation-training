29c291718fae8c7279fbadfb56cce40eda9a3fb2
Alexey Krytsky <alexeykrytsky@gmail.com>
2019-12-24 14:46:09 +0300

    framework2: fix SignInPage selectors

--- webdriver/src/test/java/edu/kriale/webdriver/pageobject/SignInPage.java
+++ webdriver/src/test/java/edu/kriale/webdriver/pageobject/SignInPage.java
@@ -7,13 +7,13 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SignInPage extends AbstractPage {
-    @FindBy(xpath = "//input[@id='gss-signin-email']")
+    @FindBy(xpath = "//input[@id='signin-loginid']")
     private WebElement emailField;
 
-    @FindBy(xpath = "//input[@id='gss-signin-password']")
+    @FindBy(xpath = "//input[@id='signin-password']")
     private WebElement passwordField;
 
-    @FindBy(xpath = "//*[@id='gss-signin-submit']")
+    @FindBy(xpath = "//*[@id='submitButton']")
     private WebElement submitButton;
 
     public SignInPage() {
